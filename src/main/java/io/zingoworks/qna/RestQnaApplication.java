bf1069bd3d74f4ce819398351745619f621d20be
JINSAN NOH <zingoworks@gmail.com>
2019-04-30 20:20:42 +0900

    Init project setting

---
+++ src/main/java/io/zingoworks/qna/RestQnaApplication.java
@@ -0,0 +1,13 @@
+package io.zingoworks.qna;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestQnaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestQnaApplication.class, args);
+	}
+
+}
